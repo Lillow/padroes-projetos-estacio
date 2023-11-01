59b49305c1360f1b382e1655521431581461a8d5
Lillow <danillordm@gmail.com>
2023-11-01 12:14:40 -0300

    Aula 10 atividade verificadora de aprendizagem

---
+++ aula10_atividade_verificadora_de_aprendizagem/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module aula10_atividade_verificadora_de_aprendizagem {
+	requires java.sql;
+}
---
+++ aula10_atividade_verificadora_de_aprendizagem/src/product/ProductDAO.java
@@ -0,0 +1,71 @@
+package product;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO {
+	private Connection connection;
+
+	public ProductDAO() {
+		// Inicialize a conexão com o banco de dados aqui (substitua as configurações).
+		try {
+			String url = "jdbc:mysql://localhost:3306/mydatabase";
+			String user = "username";
+			String password = "password";
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Product> getAllProducts() {
+		List<Product> products = new ArrayList<>();
+		try {
+			String sql = "SELECT * FROM products";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				double price = resultSet.getDouble("price");
+				Product product = new Product(id, name, price);
+				products.add(product);
+			}
+
+			resultSet.close();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return products;
+	}
+
+	public void saveProduct(Product product) {
+		try {
+			String sql = "INSERT INTO products (name, price) VALUES (?, ?)";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, product.getName());
+			statement.setDouble(2, product.getPrice());
+			statement.executeUpdate();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
